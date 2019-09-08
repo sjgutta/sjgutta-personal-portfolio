7c4e5a1157673bcbca688bb7eb05eea4d6210305
Sajan Gutta <gutta@usc.edu>
2019-09-08 16:05:17 -0700

    Implemented first version of start and add. Wrote some helper functions too

--- hw1/the_rani/the_rani.cpp
+++ hw1/the_rani/the_rani.cpp
@@ -10,7 +10,6 @@ class TheRani {
 public:
     TheRani(char* input_path, char* output_path);
     ~TheRani();
-
     // Call execute and handles exceptions
     void main();
 
@@ -18,6 +17,7 @@ private:
     int experiment_count;       // You will need to track the number of experiments
     int* subject_counts;        // For each, the number of subjects
     string** subject_history;   // And for each subject, their history
+    int subject_total;
 
     ifstream input;             // Input file stream
     ofstream output;            // Output file stream
@@ -26,9 +26,11 @@ private:
     void execute(const string& line);
 
     // Possible helper: deallocate all current members
+    void destroy_current();
 };
 
 TheRani::TheRani(char* input_path, char* output_path) : experiment_count(0), input(input_path), output(output_path) {
+    
 
 }
 
@@ -36,7 +38,25 @@ TheRani::~TheRani() {
 
 }
 
+//deallocates all dynamically allocated arrays
+void TheRani::destroy_current(){
+    if subject_history != NULL{
+        for(int i=0;i<experiment_count;i++){
+            delete [] subject_history[i];
+        }
+        delete [] subject_history
+    }
+    if subject_counts != NULL{
+        delete [] subject_counts;
+    }
+}
 // Possible helper: read an integer argument from a stream
+void readInt(stringsteam stream){
+    int temp;
+    stream >> temp;
+    return temp;
+}
+
 
 void TheRani::main() {
     string line;
@@ -61,11 +81,55 @@ void TheRani::execute(const string& line) {
         if (subject_pool_count < 0) {
             throw out_of_range("argument out of range");
         }
-
+        subject_total = subject_pool_count;
         // Your code here
+        if subject_history{
+            destroy_current();
+        }
+        subject_history = new string*[experiment_count+1];
+        subject_history[0] = new string[subject_pool_count];
+        subject_counts = new int[experiment_count+1];
+        subject_counts[0] = subject_pool_count;
+    } else if (command == "ADD"){
+        //incrementing the number of experiments
+        experiment_count++;
+        //creating temporary new arrays for the revised experiment set
+        string** temp_subject_history = new string*[experiment_count+1];
+        for(int i=0; i<experiment_count+1; i++){
+            temp_subject_history[i] = new string[subject_total];
+        }
+        int* temp_subject_counts = new int[experiment_count+1];
+        //copying over old experiment set info.
+        //loop through old subject history and into the temp one
+        for(int i=0; i<experiment_count; i++){
+            for(int j=0; j<subject_counts[i];j++){
+                temp_subject_history[i][j] = subject_history[i][j];
+            }
+        }
+        for(int i=0; i<experiment_count;i++){
+            temp_subject_counts[i] = subject_counts[i];
+        }
+        //setting new experiment subject count to 0
+        temp_subject_counts[experiment_count-1] = 0;
+        //deleting old experiment set with helper function
+        destroy_current();
+        //setting data members equal to new experiment set
+        subject_history = temp_subject_history;
+        subject_counts = temp_subject_counts;
+    } else if (command == "MOVE"){
+        //getting arguments into variables
+        int experiment1 = readInt(stream);
+        int experiment2 = readInt(stream);
+        int sublow = readInt(stream);
+        int subhigh = readInt(stream);
 
-    } // else if (more conditions) { ...
+    } else if (command == "QUERY"){
+        
+    }else{
+        
+    }// else if (more conditions) { ...
 }
+
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
