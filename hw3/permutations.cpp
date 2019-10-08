039d89f40cd8ae6ba609987999fd22e7eb1266fd
Sajan Gutta <gutta@usc.edu>
2019-10-07 20:23:47 -0700

    implemented permutations recursive function, added readme and text file for answers to git

---
+++ hw3/permutations.cpp
@@ -0,0 +1,23 @@
+/*This program is written for Homework #3, part 4
+The program takes in a string and prints out
+all possible permutations of the string*/
+
+#include <string>
+#include <iostream>
+
+void permutationsHelper(std::string in, std::string curr){
+    if(int(in.length())==0){
+        std::cout << curr << std::endl;
+        return;
+    }
+    for(int i=0; i<int(in.length()); i++){
+        std::string temp = in.substr(0,i)+in.substr(i+1,in.length()-1);
+        permutationsHelper(temp, curr+in[i]);
+    }
+}
+
+void permutations(std::string in){
+    if(int(in.length())>0){
+        permutationsHelper(in,"");
+    }
+}
