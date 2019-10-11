c4b23672a3c77fb3756d1afb81f5244937c6a7bd
Sajan Gutta <gutta@usc.edu>
2019-10-11 15:47:02 -0700

    implementing lots of company tracker functions

--- hw3/company.cpp
+++ hw3/company.cpp
@@ -13,6 +13,12 @@ CompanyTracker::~CompanyTracker ()
   // deallocates all dynamically allocated memory
 {
   // your implementation goes here
+  for(int i=0; i<numCompanies; i++){
+    while(findLargestCompany(i) != companies[i]){
+      split(i);
+    }
+    delete companies[i];
+  }
 }
 
 void CompanyTracker::merge (int i, int j)
@@ -26,6 +32,23 @@ void CompanyTracker::merge (int i, int j)
      If either i or j are out of range, merge doesn't do anything. */
 {
   // your implementation goes here
+  if(i<0||i>=numCompanies){
+    return;
+  }
+  if(j<0||j>=numCompanies){
+    return;
+  }
+  if(i==j){
+    return;
+  }
+  Company* company1 = findLargestCompany(i);
+  Company* company2 = findLargestCompany(j);
+  if(company1==company2){
+    return;
+  }
+  Company* new_parent = new Company(company1,company2);
+  company1->parent==new_parent;
+  company2->parent==new_parent;
 }
 
 void CompanyTracker::split (int i)
@@ -37,6 +60,16 @@ void CompanyTracker::split (int i)
      If i is out of range, split doesn't do anything. */
 {
   // your implementation goes here
+  if(i<0||i>=numCompanies){
+    return;
+  }
+  Company* largest = findLargestCompany(i);
+  if(largest->merge1==nullptr){
+    return;
+  }
+  largest->merge1->parent=nullptr;
+  largest->merge2->parent=nullptr;
+  delete largest;
 }
 
 bool CompanyTracker::inSameCompany (int i, int j)
@@ -45,8 +78,25 @@ bool CompanyTracker::inSameCompany (int i, int j)
      Returns false if i or j (or both) is out of range. */
 {
   // your implementation goes here
+  if(i<0||i>=numCompanies){
+    return false;
+  }
+  if(j<0||j>=numCompanies){
+    return false;
+  }
+  if(i==j){
+    return true;
+  }else if(companies[i]->parent==companies[j]->parent && companies[i]->parent){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 Company* CompanyTracker::findLargestCompany(int i){
-  
+  Company* result = companies[i];
+  while(result->parent){
+    result = result->parent;
+  }
+  return result;
 }
