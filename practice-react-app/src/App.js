458964710bea9f7aeb2d867e28469f42c25af530
Sajan Gutta <gutta@usc.edu>
2020-11-30 08:35:06 -0500

    implementing first useState hook for displaying search status, using callback function in input field to capture changes in search value

--- practice-react-app/src/App.js
+++ practice-react-app/src/App.js
@@ -1,39 +1,51 @@
 import './App.css';
+import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectId: 1
-  }
-]
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectId: 1
+    }
+  ];
 
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
+  const [searchTerm, setSearchTerm] = React.useState('');
 
-    <hr />
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
 
-    <List />
-  </div>
-);
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-const List = () =>
-  list.map(item => (
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>Searching for <strong>{searchTerm}</strong>.</p>
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+};
+
+const List = props =>
+  props.list.map(item => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
