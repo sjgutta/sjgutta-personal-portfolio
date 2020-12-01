825f598b3667ed7fd28325c465102df571a9f5a2
Sajan Gutta <gutta@usc.edu>
2020-12-01 17:19:33 -0500

    simulating asynchronous data handling in React with a hardcoded Promise

--- practice-react-app/src/App.js
+++ practice-react-app/src/App.js
@@ -20,6 +20,14 @@ const initialStories = [
   }
 ];
 
+const getAsyncStories = () =>
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(localStorage.getItem('key') || initialState);
 
@@ -33,7 +41,13 @@ const useSemiPersistentState = (key, initialState) => {
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = item => {
     const newStories = stories.filter(
