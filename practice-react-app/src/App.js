169d76ee9029842e9271add3eb25140813fc49a2
Sajan Gutta <gutta@usc.edu>
2020-12-02 09:30:24 -0500

    adding styling for app component using standard inline CSS and styling with classes

--- practice-react-app/src/App.js
+++ practice-react-app/src/App.js
@@ -82,16 +82,14 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-
-      <hr />
 
       {stories.isError && <p>Something went wrong ...</p>}
 
@@ -109,7 +107,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -119,7 +117,7 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type="submit" disabled={!searchTerm}>
+    <button type="submit" disabled={!searchTerm} className="button button_large">
       Submit
     </button>
   </form>
@@ -136,7 +134,7 @@ const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, chil
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">{children}</label>
       &nbsp;
       <input
         ref={inputRef}
@@ -144,6 +142,7 @@ const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, chil
         type={type}
         value={value}
         onChange={onInputChange}
+        className="input"
       />
     </>
   );
@@ -153,15 +152,17 @@ const List = ({ list, onRemoveItem }) =>
   list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />);
 
 const Item = ({ item, onRemoveItem }) => (
-  <div>
-    <span>
+  <div className="item">
+    <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button type="button" onClick={() => onRemoveItem(item)} className="button button_small">
+        Dismiss
+      </button>
     </span>
   </div>
 );
