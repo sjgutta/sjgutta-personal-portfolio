206f5f5b8dddc022680f4fa34131b896cd2d8e25
Sajan Gutta <gutta@usc.edu>
2020-12-03 10:29:52 -0500

    switching back to normal CSS styling with class names, using first svg in application with svg as a React Component

--- practice-react-app/src/App.js
+++ practice-react-app/src/App.js
@@ -1,129 +1,68 @@
-import styles from './App.module.css';
 import React from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+import './App.css'
+import { ReactComponent as Check } from './check.svg'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
-  const [value, setValue] = React.useState(localStorage.getItem('key') || initialState);
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
 
   React.useEffect(() => {
-    localStorage.setItem('key', value)
+    localStorage.setItem(key, value);
   }, [value, key]);
 
   return [value, setValue];
-}
+};
 
-const storiesReducer = (state, action) =>
-{
+const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
-      return {...state, isLoading: true, isError: false};
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
     case 'STORIES_FETCH_SUCCESS':
-      return {...state, isLoading: false, isError: false, data: action.payload};
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
     case 'STORIES_FETCH_FAILURE':
-      return {...state, isLoading: false, isError: true};
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
     case 'REMOVE_STORY':
-      return {...state, 
+      return {
+        ...state,
         data: state.data.filter(
           story => action.payload.objectID !== story.objectID
-        )
+        ),
       };
     default:
       throw new Error();
   }
 };
 
-const StyledContainer = styled.div`
-  height: 100vw;
-  padding: 20px;
-
-  background: #83a4d4;
-  background: linear-gradient(to left, #b6fbff, #83a4d4);
-
-  color: #171212;
-`;
-
-const StyledHeadlinePrimary = styled.h1`
-  font-size: 48px;
-  font-weight: 300;
-  letter-spacing: 2px;
-`;
-
-const StyledItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding-bottom: 5px;
-`;
-
-const StyledColumn = styled.span`
-  padding: 0 5px;
-  white-space: nowrap;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-
-  a {
-    color: inherit;
-  }
-
-  width: ${props => props.width};
-`;
-
-const StyledButton = styled.button`
-  background: transparent;
-  border: 1px solid #171212;
-  padding: 5px;
-  cursor: pointer;
-
-  transition: all 0.1s ease-in;
-
-  &:hover {
-    background: #171212;
-    color: #ffffff;
-  }
-`;
-
-const StyledButtonSmall = styled(StyledButton)`
-  padding: 5px;
-`;
-
-const StyledButtonLarge = styled(StyledButton)`
-  padding: 10px;
-`;
-
-const StyledSearchForm = styled.form`
-  padding: 10px 0 20px 0;
-  display: flex;
-  align-items: baseline;
-`;
-
-const StyledLabel = styled.label`
-  border-top: 1px solid #171212;
-  border-left: 1px solid #171212;
-  padding-left: 5px;
-  font-size: 24px;
-`;
-
-const StyledInput = styled.input`
-  border: none;
-  border-bottom: 1px solid #171212;
-  background-color: transparent;
-
-  font-size: 24px;
-`;
-
 const App = () => {
-  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
 
   const [url, setUrl] = React.useState(
     `${API_ENDPOINT}${searchTerm}`
   );
 
   const [stories, dispatchStories] = React.useReducer(
-    storiesReducer, 
-    {data : [], isLoading: false, isError: false }
+    storiesReducer,
+    { data: [], isLoading: false, isError: false }
   );
 
   const handleFetchStories = React.useCallback(async () => {
@@ -148,7 +87,7 @@ const App = () => {
   const handleRemoveStory = item => {
     dispatchStories({
       type: 'REMOVE_STORY',
-      payload: item
+      payload: item,
     });
   };
 
@@ -158,12 +97,13 @@ const App = () => {
 
   const handleSearchSubmit = event => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+
     event.preventDefault();
   };
 
   return (
-    <StyledContainer>
-      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -175,10 +115,10 @@ const App = () => {
 
       {stories.isLoading ? (
         <p>Loading ...</p>
-      ): (
+      ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </StyledContainer>
+    </div>
   );
 };
 
@@ -187,7 +127,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <StyledSearchForm onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -197,13 +137,24 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <StyledButtonLarge type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
-    </StyledButtonLarge>
-  </StyledSearchForm>
+    </button>
+  </form>
 );
 
-const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, children }) => {
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
   const inputRef = React.useRef();
 
   React.useEffect(() => {
@@ -214,39 +165,49 @@ const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, chil
 
   return (
     <>
-      <StyledLabel htmlFor={id}>{children}</StyledLabel>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
       &nbsp;
-      <StyledInput
+      <input
         ref={inputRef}
         id={id}
         type={type}
         value={value}
         onChange={onInputChange}
+        className="input"
       />
     </>
   );
 };
 
 const List = ({ list, onRemoveItem }) =>
-  list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />);
+  list.map(item => (
+    <Item
+      key={item.objectID}
+      item={item}
+      onRemoveItem={onRemoveItem}
+    />
+  ));
 
 const Item = ({ item, onRemoveItem }) => (
-  <StyledItem>
-    <StyledColumn width="40%">
+  <div className="item">
+    <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
-    </StyledColumn>
-    <StyledColumn width="30%">{item.author}</StyledColumn>
-    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
-    <StyledColumn width="10%">{item.points}</StyledColumn>
-    <StyledColumn width="10%">
-      <StyledButtonSmall
+    </span>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
         type="button"
         onClick={() => onRemoveItem(item)}
+        className="button button_small"
       >
-        Dismiss
-      </StyledButtonSmall>
-    </StyledColumn>
-  </StyledItem>
+        <Check height="18px" width="18px" />
+      </button>
+    </span>
+  </div>
 );
 
 export default App;
