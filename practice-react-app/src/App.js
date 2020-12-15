4a51036f68e685339a50daf972ed3bd799798a43
Sajan Gutta <gutta@usc.edu>
2020-12-15 13:27:09 -0500

    implementing reverse sorting option if button clicked again

--- practice-react-app/src/App.js
+++ practice-react-app/src/App.js
@@ -203,14 +203,22 @@ const SORTS = {
 };
 
 const List = React.memo(({ list, onRemoveItem }) => {
-  const [sort, setSort] = React.useState('NONE');
+  const [sort, setSort] = React.useState({
+    sortKey: 'NONE',
+    isReverse: false,
+  });
 
   const handleSort = sortKey => {
-    setSort(sortKey);
+    const isReverse = sort.sortKey === sortKey && !sort.isReverse;
+
+    setSort({ sortKey, isReverse });
   };
 
-  const sortFunction = SORTS[sort];
-  const sortedList = sortFunction(list);
+  const sortFunction = SORTS[sort.sortKey];
+
+  const sortedList = sort.isReverse
+    ? sortFunction(list).reverse()
+    : sortFunction(list);
 
   return (
     <div>
